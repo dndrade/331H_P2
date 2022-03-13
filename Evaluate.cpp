121769f07542b09dddd12ee247f778e00589a13c
Thammy <thamires.lopesdeandrade@stu.bmcc.cuny.edu>
2022-03-13 04:08:16 -0400

    New class

--- Evaluate.cpp
+++ Evaluate.cpp
@@ -12,5 +12,19 @@ Evaluate<T>::~Evaluate() {
 
 template <class T>
 int Evaluate<T>::operatorOrder(char optr) {
-
+    // exponents
+    if (optr == '^') {
+        return 3;
+    }
+    // division and multiplication are the same level
+    else if (optr == '/' || optr == '*') { 
+        return 2;
+    } 
+    // addition and subtraction are the same level
+    else if (optr == '+' || optr == '-') {
+        return 1;
+    }
+    else {
+        return -1;
+    }
 }
