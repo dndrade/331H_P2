4b627ea8662334b8f908fcaaef9a8cda66fa4774
ladyomega <68204412+ladyomega@users.noreply.github.com>
2022-03-14 02:01:20 -0400

    isOperator

--- Evaluate.h
+++ Evaluate.h
@@ -15,8 +15,10 @@ public:
 	~Evaluate();
 
 	void setExpression(std::string expr); // user's expression to be evaluated
+
 	int operatorOrder(char optr); // returns the precedence
 	int operation(Stack<char> operators, Stack<int> operands); // performs arithmetic operations
+	bool isOperator(char optr) // returts true for ^ * / + -
 
 
 };
